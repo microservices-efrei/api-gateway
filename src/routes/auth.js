b036b7f28dc034fac18a339831059b43cbd6d346
Louis <roccalouis26@gmail.com>
2024-12-10 17:08:38 +0100

    gateway wip

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const createServiceProxy = require("../utils/proxy");
 
 const router = express.Router();
-const userServiceProxy = createServiceProxy("http://user-service:3001/api");
+const userServiceProxy = createServiceProxy("http://user-service:3001/");
 
 router.use("/", userServiceProxy);
 
